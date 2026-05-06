d9452ba753e2576a0d7d107f326146ee71e3e10f
fansypants <balthasarspann@gmail.com>
2026-05-06 16:04:35 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,10 @@
-const CACHE = 'buzz-v1';
-const FILES = ['/', '/buzz/', '/buzz/index.html', '/buzz/manifest.json', '/buzz/icon-192.png', '/buzz/icon-512.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(() => {})));
-  self.skipWaiting();
-});
-
+// Network-first: always try to get fresh content, fall back to cache
+self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
   );
-});
-
-self.addEventListener('message', e => {
-  if (e.data?.type === 'SKIP_WAITING') self.skipWaiting();
 });
